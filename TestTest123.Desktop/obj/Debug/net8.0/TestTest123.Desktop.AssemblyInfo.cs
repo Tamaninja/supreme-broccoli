5b5cb6139a22b9c135c7f194b408f8a300dee8e6
Tamaninja <liel.king54@gmail.com>
2024-05-04 17:28:54 +0300

    implemented wasapi audio still bfoken

--- TestTest123.Desktop/obj/Debug/net8.0/TestTest123.Desktop.AssemblyInfo.cs
+++ TestTest123.Desktop/obj/Debug/net8.0/TestTest123.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestTest123")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+aa5818e58f2eb0b719b47e3805fab2666caf6179")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+56cfe6d1d2c432a49b6c2c76d99ffaaffc72fe54")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestTest123")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestTest123")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
--- TestTest123.Game/Speaker.cs
+++ TestTest123.Game/Speaker.cs
@@ -1,14 +1,17 @@
 ﻿
 using System;
-using System.Runtime.InteropServices;
-using HidSharp.Reports.Units;
+using System.Threading;
+using System.Threading.Tasks;
 using ManagedBass;
-using ManagedBass.Mix;
+using ManagedBass.Wasapi;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Logging;
+using osu.Framework.Threading;
 
 
 namespace TestTest123.Game
@@ -16,41 +19,45 @@ namespace TestTest123.Game
     public partial class Speaker : DrawableAudioWrapper
     {
         private SpriteText spriteText;
-        
-        public Speaker(SpriteText text) : base(new Container()) {
-            spriteText = text;
+        private int playbackDevice;
+        private int recordingDevice;
+        private     WasapiProcedure wasapiProcedure;
 
-            Start();
+
+        public Speaker(SpriteText text) : base(new Container())
+        {
+            spriteText = text;
         }
 
 
         [BackgroundDependencyLoader]
         private void load(AudioManager audioManager)
         {
-            Bass.Init(-1, 48000, DeviceInitFlags.Hog);
+            AudioInputThread audioInputThread = new AudioInputThread();
+
+            wasapiProcedure = (buffer, length, user) =>
+            {
+                audioInputThread.Playback(buffer, length, user);
+
+                return (length);
+            };
+
+
+            /*            for (int i = 0; i < BassWasapi.DeviceCount; i++)
+                        {
+                            Logger.LogPrint(i + "\t" + BassWasapi.GetDeviceInfo(i).ToString());
+                            Thread.Sleep(150);
+
+                        }*/
+
+
+            Logger.LogPrint("device init: " + BassWasapi.Init(-2, 48000, Procedure: wasapiProcedure));
+            Logger.LogPrint("Format: " + BassWasapi.Info.Frequency);
+
+            Logger.LogPrint("device start: " + BassWasapi.Start());
+            Logger.LogPrint("device: " + BassWasapi.CurrentDevice);
+
         }
+    }
 
-        private const int DefaultFrequency = 48000; // Standard audio sample rate
-
-        public void Start()
-        {
-            Bass.RecordInit(4);
-            Bass.RecordGetDeviceInfo(4);
-            Bass.Configure(Configuration.PlaybackBufferLength, 10);
-            Bass.Configure(Configuration.UpdatePeriod, 5);
-
-            int recordHandle = Bass.RecordStart(DefaultFrequency, 2,BassFlags.Default , myRecordingCallback);
-            int sampleHandle = Bass.CreateStream(DefaultFrequency, 2, BassFlags.Default, StreamProcedureType.Push);
-            Bass.ChannelPlay(sampleHandle);
-
-        }
-
-        private bool myRecordingCallback(int handle, IntPtr buffer, int length, IntPtr user)
-        {
-
-            Bass.StreamPutData(sampleHandle, buffer, length);
-
-            return (true);
-        }
-    }       
 }
